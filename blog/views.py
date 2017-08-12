c5c0b4feca2ab4d77f3977ca326118d56a05d61d
Howl <tun224813@gmail.com>
2017-08-12 21:47:52 +0800

    fix unshow markdown

--- blog/views.py
+++ blog/views.py
@@ -9,7 +9,7 @@ from django.shortcuts import render, get_object_or_404
 
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    post.body = markdown.markdown(post.body, [
+    post.body = markdown.markdown(post.body, extensions=[
         'markdown.extensions.extra',
         'markdown.extensions.codehilite',
         'markdown.extensions.toc',
